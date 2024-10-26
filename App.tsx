3d75c38df277b11a61b01978005e3246ee09e85d
Ranna <rannamead58@gmail.com>
2024-10-25 22:59:33 -0300

    app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,38 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Home } from "./src/screens/Home";
+import "react-native-gesture-handler";
+import {
+  matchQuery,
+  MutationCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import Toast from "react-native-toast-message";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-const queryClient = new QueryClient();
+import { Home } from "./src/screens/Home/Home";
+
+const queryClient = new QueryClient({
+  mutationCache: new MutationCache({
+    onSuccess: (_data, _variables, _context, mutation) => {
+      queryClient.refetchQueries({
+        predicate: (query) =>
+          mutation.meta?.refetches?.some((queryKey) =>
+            matchQuery({ queryKey }, query)
+          ) ?? false,
+      });
+    },
+  }),
+});
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Home />
-    </QueryClientProvider>
+    <SafeAreaProvider style={{ backgroundColor: "#fff", flex: 1 }}>
+      <GestureHandlerRootView>
+        <QueryClientProvider client={queryClient}>
+          <Home />
+          <Toast />
+        </QueryClientProvider>
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 }
